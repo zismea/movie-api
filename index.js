0487139103c1e534dc2fcdb2879e799e1188262c
zismea <kacp.ziemba@gmail.com>
2024-12-08 14:41:54 +0100

    deleted cors

--- index.js
+++ index.js
@@ -9,22 +9,6 @@ const Movies = Models.Movie;
 const Users = Models.User;
 const { check, validationResult } = require('express-validator');
 
-// Run CORS
-/* let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
-
-
-const cors = require('cors');
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-})); */
-
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Middleware to log all requests and parse JSON
