5979e2e912dffb6443f9582d697d9fac046868a7
zismea <kacp.ziemba@gmail.com>
2024-12-08 14:20:06 +0100

    cors taken out

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ const mongoose = require('mongoose');
 const Models = require('./models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
-const cors = require('cors');
 const { check, validationResult } = require('express-validator');
 
 // Run CORS
 /* let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
 
+
+const cors = require('cors');
 app.use(cors({
   origin: (origin, callback) => {
     if(!origin) return callback(null, true);
